c6fa17e3f6a40684fe8fdfb2216c58a390497763
Ronny Seiger <ronny.seiger@unisg.ch>
2023-04-26 09:57:03 +0200

    Minor fixes

--- scripts/create-topics.sh
+++ scripts/create-topics.sh
@@ -1,17 +1,7 @@
 echo "Waiting for Kafka to come online..."
 cub kafka-ready -b kafka:9092 1 20
 # create the src-topic topic
-kafka-topics \
-  --bootstrap-server kafka:9092 \
-  --topic src-topic \
-  --replication-factor 1 \
-  --partitions 1 \
-  --create
+kafka-topics --create --bootstrap-server kafka:9092 --topic src-topic --replication-factor 1 --partitions 1
 # create the out-topic topic
-kafka-topics \
-  --bootstrap-server kafka:9092 \
-  --topic out-topic \
-  --replication-factor 1 \
-  --partitions 1 \
-  --create
+kafka-topics --create --bootstrap-server kafka:9092 --topic out-topic --replication-factor 1 --partitions 1
 sleep infinity
